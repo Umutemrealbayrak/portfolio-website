f8f24a996e3101b871d64e9eb6ce4d0a9b25c090
Umutemrealbayrak <albayrakumut727@gmail.com>
2026-05-15 22:23:26 +0300

    Proje güncellendi

---
+++ main-site/main-site/src/pages/Dekupe.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Dekupe() {
+  const [subCategories, setSubCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:7148/api/images/sub-categories/1") // 1 = dekupe
+      .then((res) => setSubCategories(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+
+      <h2 className="text-center fw-bold mb-4">Dekupe</h2>
+
+      <div className="row g-4">
+        {subCategories.map((x) => (
+          <div className="col-md-3" key={x.Id}>
+            <Link
+              to={`/dekupe/${x.Id}`}
+              className="text-decoration-none"
+            >
+              <div className="card shadow-sm text-center p-4 h-100">
+
+                <h5 className="fw-bold">{x.Name}</h5>
+
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default Dekupe;
