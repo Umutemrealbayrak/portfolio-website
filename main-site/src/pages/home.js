2fcac701f15cfdb7c088c769be3c73f05670c940
Umutemrealbayrak <albayrakumut727@gmail.com>
2026-05-16 02:23:07 +0300

    Update frontend and main-site

--- main-site/src/pages/home.js
+++ main-site/src/pages/home.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
 import logo from "../assets/indir1.jpeg"; // logonun yolu
-import ai from "../assets/Adobe_Illustrator_CC_icon.svg.png";
-import ps from "../assets/Adobe_Photoshop_CC_icon.svg.png";
-import figma from "../assets/Adobe_Photoshop_Lightroom_CC_logo.svg.png";
-import ae from "../assets/Adobe_After_Effects_CC_icon.svg.png";
-import pr from "../assets/Adobe_Premiere_Pro_CC_icon.svg.png";
-
 
 function Home() {
   return (
@@ -16,12 +10,12 @@ function Home() {
       <div className="top-center-container">
         <img src={logo} alt="Logo" className="top-logo" />
         <div className="circles-container">
-  <div className="circle" style={{ backgroundImage: `url(${ps})` }}></div>
-  <div className="circle" style={{ backgroundImage: `url(${figma})` }}></div>
-  <div className="circle" style={{ backgroundImage: `url(${ai})` }}></div>
-  <div className="circle" style={{ backgroundImage: `url(${ae})` }}></div>
-  <div className="circle" style={{ backgroundImage: `url(${pr})` }}></div>
-</div>
+          <div className="circle" img src="C:\Users\albay\Desktop\ersindekupe\main-site\src\assets\Adobe_Illustrator_CC_icon.svg.png"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+        </div>
       </div>
 
       {/* Sürekli dönen banner alanları */}
